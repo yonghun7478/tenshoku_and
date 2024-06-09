bb6cad56123ea54e4ac307efaaa31358699bb08d
yong <yonghun7478@gmail.com>
2024-06-09 15:18:25 +0900

    基本のコードの作成

--- app/src/main/java/com/example/tenshoku_and/MainActivity.kt
+++ app/src/main/java/com/example/tenshoku_and/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.tenshoku_and.ui.screen.MainScreen
 import com.example.tenshoku_and.ui.theme.Tenshoku_andTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,6 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Tenshoku_andTheme {
+                MainScreen()
             }
         }
     }
---
+++ app/src/main/java/com/example/tenshoku_and/ui/screen/MainScreen.kt
@@ -0,0 +1,15 @@
+package com.example.tenshoku_and.ui.screen
+
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+@Composable
+fun MainScreen() {
+    MainContent()
+}
+@Composable
+fun MainContent(
+    modifier: Modifier = Modifier()
+) {
+
+}
