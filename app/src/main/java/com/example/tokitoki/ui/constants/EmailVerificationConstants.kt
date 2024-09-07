049b2871e7b36c7a6f0510e36e4146f861bbeb5e
yong <yonghun7478@gmail.com>
2024-09-08 02:08:22 +0900

    EmailVerificationScreen.ktの基盤コード作成

--- app/src/main/java/com/example/tokitoki/ui/constants/EmailVerificationConstants.kt
+++ app/src/main/java/com/example/tokitoki/ui/constants/EmailVerificationConstants.kt
@@ -1,5 +1,8 @@
 package com.example.tokitoki.ui.constants
 
+class EmailVerificationConstants {
+}
+
 sealed class EmailVerificationAction {
     data object Nothing : EmailVerificationAction()
     data object SUBMIT : EmailVerificationAction()
