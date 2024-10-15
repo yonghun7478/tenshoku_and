7b884b825d5d2f4083d303a2932c6c3fac1480ba
yong <yonghun7478@gmail.com>
2024-10-15 22:51:11 +0900

    fake repo

--- app/src/androidTest/java/com/example/tokitoki/fake/repo/FakeUserInterestRepositoryImpl.kt
+++ app/src/androidTest/java/com/example/tokitoki/fake/repo/FakeUserInterestRepositoryImpl.kt
@@ -21,6 +21,11 @@ class FakeUserInterestRepositoryImpl @Inject constructor(
                     UserInterestWithCategoryEntity(interestId = 3, interestTitle = "ヨガ3", interestUrl = "https://www.dabur.com/Blogs/Doshas/Importance%20and%20Benefits%20of%20Yoga%201020x450.jpg", categoryId = 1, categoryTitle = "趣味"),
                     UserInterestWithCategoryEntity(interestId = 4, interestTitle = "Hobby Crafting", interestUrl = "https://example.com/hobby4", categoryId = 1, categoryTitle = "趣味"),
                     UserInterestWithCategoryEntity(interestId = 5, interestTitle = "Hobby Gaming", interestUrl = "https://example.com/hobby5", categoryId = 1, categoryTitle = "趣味"),
+                    UserInterestWithCategoryEntity(interestId = 6, interestTitle = "ヨガ1", interestUrl = "https://www.dabur.com/Blogs/Doshas/Importance%20and%20Benefits%20of%20Yoga%201020x450.jpg", categoryId = 1, categoryTitle = "趣味"),
+                    UserInterestWithCategoryEntity(interestId = 7, interestTitle = "ヨガ2", interestUrl = "https://www.dabur.com/Blogs/Doshas/Importance%20and%20Benefits%20of%20Yoga%201020x450.jpg", categoryId = 1, categoryTitle = "趣味"),
+                    UserInterestWithCategoryEntity(interestId = 8, interestTitle = "ヨガ3", interestUrl = "https://www.dabur.com/Blogs/Doshas/Importance%20and%20Benefits%20of%20Yoga%201020x450.jpg", categoryId = 1, categoryTitle = "趣味"),
+                    UserInterestWithCategoryEntity(interestId = 9, interestTitle = "Hobby Crafting", interestUrl = "https://example.com/hobby4", categoryId = 1, categoryTitle = "趣味"),
+                    UserInterestWithCategoryEntity(interestId = 10, interestTitle = "Hobby Gaming", interestUrl = "https://example.com/hobby5", categoryId = 1, categoryTitle = "趣味"),
                 )
             }
             2 -> {
