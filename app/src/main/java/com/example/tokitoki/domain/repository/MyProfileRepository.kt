ccaab43f305b65b9740120d014d9d216e4411990
yong <yonghun7478@gmail.com>
2024-12-21 11:52:15 +0900

    不要repository修正

--- app/src/main/java/com/example/tokitoki/domain/repository/MyProfileRepository.kt
+++ app/src/main/java/com/example/tokitoki/domain/repository/MyProfileRepository.kt
@@ -7,10 +7,6 @@ interface MyProfileRepository {
     // User Profile 관련 메서드
     suspend fun getUserProfile(): MyProfile? // 유저 프로필 조회
     suspend fun saveUserProfile(profile: MyProfile) // 유저 프로필 저장 (업데이트 포함)
-    suspend fun updateUserName(name: String) // 유저 이름 변경
-    suspend fun updateUserBirthday(birthDay: String) // 유저 나이 변경
-    suspend fun updateGender(isMale: Boolean)
-    suspend fun updateMySelfSentence(sentenceId: Int) // 자기소개 변경
     suspend fun deleteUserProfile() // 유저 프로필 삭제
 
     // User Tags 관련 메서드
