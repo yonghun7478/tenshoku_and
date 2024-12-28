3fea06eb779d5050c7f877de13452777100ec415
yonghun <yonghun7478@gmail.com>
2024-12-28 15:53:31 +0900

    Credential 追加

--- app/src/main/java/com/example/tokitoki/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/example/tokitoki/domain/repository/AuthRepository.kt
@@ -1,12 +1,14 @@
 package com.example.tokitoki.domain.repository
 
 import com.example.tokitoki.common.ResultWrapper
+import com.example.tokitoki.domain.model.Credential
 import com.example.tokitoki.domain.model.MyProfile
 import com.example.tokitoki.domain.model.Tokens
 
 interface AuthRepository {
     suspend fun sendVerificationCode(email: String, code: String): ResultWrapper<Tokens>
     suspend fun registerMyProfile(myProfile: MyProfile, thumbnailPath: String): ResultWrapper<MyProfile>
+    suspend fun signWithGoogle(): Credential
 
     fun saveTokens(token: String, refreshToken: String)
     fun getTokens(): Tokens
