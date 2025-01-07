9c3adb264ec856b9fc0dc84bd94daff3f979e548
yonghun <yonghun7478@gmail.com>
2025-01-07 13:58:23 +0900

    templateの作成

--- app/src/main/java/com/example/tokitoki/ui/screen/MainScreen.kt
+++ app/src/main/java/com/example/tokitoki/ui/screen/MainScreen.kt
@@ -1,9 +1,123 @@
 package com.example.tokitoki.ui.screen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import com.example.tokitoki.ui.state.MainBottomItem
+import com.example.tokitoki.ui.state.MainUiEvent
+import com.example.tokitoki.ui.state.MainUiState
+import com.example.tokitoki.ui.viewmodel.MainViewModel
 
 @Composable
-fun MainScreen() {
-    Text(text = "MAIN")
+fun MainScreen(
+    viewModel: MainViewModel = hiltViewModel(),
+) {
+    // StateFlow를 사용하여 UI 상태를 관찰
+    val uiState by viewModel.uiState.collectAsState()
+
+    // SharedFlow 이벤트 처리
+    LaunchedEffect(viewModel.uiEvent) {
+        viewModel.uiEvent.collect { event ->
+            when (event) {
+                is MainUiEvent.SelectBottomItem -> {
+
+                }
+            }
+        }
+    }
+
+    // UI 상태를 MainContents에 전달
+    MainContents(
+        uiState = uiState,
+        onEvent = { viewModel.onEvent(it) }
+    )
+}
+
+@Composable
+fun MainContents(
+    uiState: MainUiState,
+    onEvent: (MainUiEvent) -> Unit
+) {
+    Scaffold(
+        bottomBar = {
+            MainBottomNavigation(
+                selectedItem = uiState.selectedBottomItem,
+                onItemSelected = { onEvent(MainUiEvent.SelectBottomItem(it)) }
+            )
+        }
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            when (uiState.selectedBottomItem) {
+                MainBottomItem.HOME -> {
+                    MainHomeContents()
+                }
+
+                MainBottomItem.LIKE -> {
+                    MainLikeContents()
+                }
+
+                MainBottomItem.MESSAGE -> {
+                    MainMessageContents()
+                }
+
+                MainBottomItem.MY_PAGE -> {
+                    MypageContents()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainBottomNavigation(
+    selectedItem: MainBottomItem,
+    onItemSelected: (MainBottomItem) -> Unit
+) {
+    NavigationBar {
+        MainBottomItem.values().forEach { item ->
+            NavigationBarItem(
+                selected = selectedItem == item,
+                onClick = { onItemSelected(item) },
+                icon = { Icon(Icons.Default.Home, contentDescription = null) },
+                label = { Text(item.name) }
+            )
+        }
+    }
+}
+
+@Composable
+fun MainHomeContents() {
+    Text("MainHomeContents")
+}
+
+@Composable
+fun MainLikeContents() {
+    Text("MainLikeContents")
+}
+
+@Composable
+fun MainMessageContents() {
+    Text("MainMessageContents")
+}
+
+@Composable
+fun MypageContents() {
+    Text("MypageContents")
 }
---
+++ app/src/main/java/com/example/tokitoki/ui/state/MainBottomItem.kt
@@ -0,0 +1,5 @@
+package com.example.tokitoki.ui.state
+
+enum class MainBottomItem {
+    HOME, LIKE, MESSAGE, MY_PAGE
+}
