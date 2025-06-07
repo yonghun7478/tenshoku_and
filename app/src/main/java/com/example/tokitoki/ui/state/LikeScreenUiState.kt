c2156323119816a3fd18807ff285cfe678dff2de
yong <yonghun7478@gmail.com>
2025-06-07 11:30:58 +0900

    不要コーど削除

--- app/src/main/java/com/example/tokitoki/ui/state/LikeScreenUiState.kt
+++ app/src/main/java/com/example/tokitoki/ui/state/LikeScreenUiState.kt
@@ -3,7 +3,6 @@ package com.example.tokitoki.ui.state
 // Data Classes (for UiState)
 data class LikeScreenUiState(
     val receivedLikes: List<LikeItemUiState> = emptyList(),
-    val showSnackBar: Boolean = false,
     val receivedLikesIsRefreshing: Boolean = false,
     val receivedCursor: Long? = null, // 추가: 받은 좋아요 커서
 )
@@ -16,11 +15,6 @@ data class LikeItemUiState(
     val introduction: String,
     val receivedTime: Long, // 추가
     val isRefreshing: Boolean = false // 새로고침 상태 추가
-)
-
-data class DeleteItemState(
-    val itemId: String? = null, // 삭제할 아이템의 ID (null이면 삭제할 아이템이 없음을 의미)
-    val showDialog: Boolean = false // 대화상자 표시 여부
 )
 
 // Tab enum
